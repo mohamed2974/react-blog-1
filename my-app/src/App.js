5d052f8a5acb63589bd77617747ba20ae1b8c22e
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2024-12-18 21:12:04 +0100

    add theme color

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+
+let theme_color_1 = {
+  backgroundColor: "#282c34",
+  sekundColor: 'rgb(97, 218, 251)',
+  sekundColorShadow: '0px 10px 16px rgba(97, 218, 251, 0.3)'
+}
+
 let owner ='Mohamed Emran'
 function App() {
   return (
