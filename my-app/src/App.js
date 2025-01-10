d8e7c68581cff5643c5be2e79c95b75ba765d3ac
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-01-10 22:47:06 +0100

    router add to app + test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/app/Header'
 import Footer from './components/app/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 let theme_color_1 = {
   backgroundColor: "#282c34",
   textColorLight: 'rgb(255, 255, 255)',
-  sekundColor: 'rgb(97, 218, 251)',
+  sekundColor: '#61dafb',
   sekundColorShadow: '0px 10px 16px rgba(97, 218, 251, 0.3)'
 }
 
@@ -13,8 +16,14 @@ let owner ='Mohamed Emran'
 function App() {
   return (
     <div className="App">
-      <Header currentTheme={theme_color_1} />
-      {/* <Footer currentTheme={theme_color_1} owner={owner}/> */}
+      <Router>
+        <Header currentTheme={theme_color_1} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+        </Routes>
+        <Footer currentTheme={theme_color_1} owner={owner}/>
+      </Router>
     </div>
   );
 }
