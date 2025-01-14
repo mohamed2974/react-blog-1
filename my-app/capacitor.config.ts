153fe73fb644f35ff4c40e5daaef00f1883bf153
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-01-14 19:19:11 +0100

    setup app

---
+++ my-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'test-app',
+  webDir: 'build'
+};
+
+export default config;
