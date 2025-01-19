4d0abb0f9187e9db58017db7dc6abd5cdbfd1b74
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-01-19 10:01:25 +0100

    download wivify and setup mobile footer + header

--- my-app/src/components/app/Footer.js
+++ my-app/src/components/app/Footer.js
@@ -1,24 +1,44 @@
 import Uls from '../Uls-component'
 import SocialMedia from '../SocialMediaLinks'
-// import Mission from '../Mission-component'
+import { Capacitor } from '@capacitor/core';
 
 let hoverEffectFooterLists = 'relative after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-slate-400 after:scale-x-0 after:origin-left hover:after:scale-x-100 after:transition-transform after:duration-300'
 
-export default function Footer({currentTheme, owner}) {
+export default function Footer({owner}) {
+    let lists = [
+        ["Support", "Documentation", "Submit ticket", "Guides",],
+        ["Company", "About", "Blog", "Jobs",],
+        ["Legal", "Terms of service", "Privacy policy", "License",]
+    ]
+
+    const isMobileApp = Capacitor.isNativePlatform();
+
+    //* ######################### FOOTERSECTION MOBILE ######################### //
+    if (isMobileApp) {
+        let MobileList = []
+        lists.forEach((list)=>{
+            let item = list[0]
+            MobileList.push(item)
+        })
+
+        return 
+            // <footer className='px-7 py-4 select-none' style={{
+            //     backgroundColor: currentTheme.backgroundColor, 
+            //     color: currentTheme.textColor
+            // }}>
+            //     <Uls  array={MobileList} style={'justify-around mb-4'} row={true} listItemStyle={'text-sm'}/>
+            //     <SocialMedia style={'flex flex-row justify-center'} imgStyle={'mx-4'}/>
+            // </footer>
+        
+    }
+
+    //* ######################### FOOTER WEB ######################### //
     return(
-    <footer className='px-9 lg:px-11 py-14 lg:py-16 select-none' style={{
-        backgroundColor: currentTheme.backgroundColor, 
-        color: currentTheme.textColor
+    <footer className='px-4 lg:px-11 py-14 lg:py-16 select-none' style={{
+        backgroundColor: '#282c34', 
+        color: 'rgb(255, 255, 255)'
     }}>
-        {/* <div className='flex flex-col lg:flex-row justify-between py-7 lg:py-11'>
-            <h1 className='text-xl lg:text-2xl'>Logo</h1>
-            <Mission />
-        </div> */}
-        <Uls listItemStyle={`text-slate-400 mt-2 w-fit ${hoverEffectFooterLists}`} style={'flex justify-between md:justify-around flex-wrap gap-7 mb-11'} array={[
-            ["Support", "Documentation", "Submit ticket", "Guides",],
-            ["Company", "About", "Blog", "Jobs",],
-            ["Legal", "Terms of service", "Privacy policy", "License",]
-        ]}/>
+        <Uls listItemStyle={`text-slate-400 mt-2 w-fit ${hoverEffectFooterLists}`} style={'flex justify-between md:justify-around flex-wrap gap-7 mb-11'} array={lists}/>
         <SocialMedia style={'flex flex-row justify-center mb-11'} imgStyle={'mx-4'}/>
         <div className='flex justify-center'>&copy; {new Date().getFullYear()} {owner}</div>
     </footer>
--- my-app/src/components/app/Header.js
+++ my-app/src/components/app/Header.js
@@ -1,39 +1,121 @@
 import Uls from '../Uls-component'
 import MobileNav from '../Mobile-navi'
 import { NavLink } from 'react-router-dom';
+import { Capacitor } from '@capacitor/core';
+import Button from '@mui/material/Button';
+import Wave from 'react-wavify'
+
 
 let hoverEffectNavList = 'relative after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-left hover:after:scale-x-100 after:transition-transform after:duration-300'
 
-export default function Header({currentTheme}){
-    
+export default function Header(){
+    const isMobileApp = Capacitor.isNativePlatform();
+
     let list = [
         <NavLink  to='/'>home</NavLink>, 
         <NavLink  to='/service'>service</NavLink>, 
         <NavLink  to='/about'>about</NavLink>, 
         <NavLink  to='/contact'>contact</NavLink>
     ]
-
+    //* ######################### HEROSECTION MOBILE ######################### //
+    if (isMobileApp) {
+        return(
+            <HeroSection className='select-none px-7 py-4 w-full flex flex-col h-[70vh] relative overflow-hidden' style={{
+                backgroundColor: '#282c34',
+                color: 'rgb(255, 255, 255)',
+            }}>
+                <div className='flex flex-row justify-between font-medium items-center select-none absolute left-0 w-full px-7 pb-4 z-30'>
+                    <h1 className='text-xl'>Logo</h1>
+                    <MobileNav array={list}/>
+                </div>
+                <div className='h-full flex flex-col justify-center items-center text-center z-10'>
+                    <h1 className='text-4xl'>Welcome to our App</h1>
+                    <p className='text-xs my-5 px-3 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+                    <Button variant="contained" sx={{backgroundColor: '#00a0cd'}}>Services</Button>
+                </div>
+                <Waves />
+            </HeroSection>
+        )
+    }
+    //* ######################### HEADER WEB ######################### //
     return(
-        <header className='select-none w-full px-9 py-4 lg:px-20 lg:py-6 flex flex-row justify-between font-medium items-center' style={{
-            backgroundColor: currentTheme.backgroundColor,
-            boxShadow: currentTheme.sekundColorShadow,
-            color: currentTheme.textColorLight
-        }}>
-            <div className='lg:w-1/12'>
-                <h1 className='text-xl lg:text-2xl'>logo</h1>
+        <header className='flex flex-row justify-between font-medium items-center select-none px-4 py-4 w-full md:w-11/12 fixed md:px-11 md:mt-3 md:left-1/2 md:transform md:-translate-x-1/2 md:rounded-full' style={{
+            backgroundColor: '#282c34',
+            boxShadow: 'rgba(97, 218, 251, 0.1) 0px 0px 20px 2px',
+            color: 'rgb(255, 255, 255)',
+            zIndex: 100
+            }}>
+            <div className='md:w-2/12'>
+                <h1 className='text-xl md:text-2xl'>Logo</h1>
             </div>
-            <Uls style={'hidden lg:flex lg:w-2/6 '} listItemStyle={`${hoverEffectNavList} p-1`} array={list} row={true}/>
-            <div className='w-1/5 flex justify-end lg:w-1/12 '>
-                <a href='/' className='hidden w-full lg:flex lg:items-center lg:justify-between'>
-                    <span>Sign in</span> 
+            <Uls style={'hidden md:justify-between md:flex md:w-fit'} listItemStyle={`${hoverEffectNavList} p-1 md:mx-2`} array={list} row={true}/>
+            <div className='w-1/5 flex justify-end md:w-2/12 '>
+                <a href='/' className='hidden w-fit md:flex md:items-center md:justify-between'>
+                    <span className='mx-2'>Sign in</span> 
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
                         <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
                 </a>
-                <div className='lg:hidden'>
+                <div className='md:hidden'>
                     <MobileNav array={list}/>        
                 </div>
             </div>
         </header>
+    )
+}
+
+//* ######################### HEROSECTION COMPONENT ######################### //
+function HeroSection({children, style, className}){
+    return(
+        <section className={className} style={style}>
+            {children}
+        </section>
+    )
+}
+
+// * ######################### WAVES COMPONENT ######################### //
+function Waves(){
+    return(
+        <div>
+            <div className='absolute left-0 bottom-0 w-full opacity-70'>
+                <Wave
+                    fill="#023047"
+                    paused={false}
+                    style={{ display: 'flex'}}
+                    options={{
+                        height: 30, 
+                        amplitude: 20, 
+                        speed: 0.15,
+                        points: 4,
+                    }}
+                />
+            </div>
+            <div className='absolute left-0 bottom-0 w-full opacity-30'>
+                <Wave
+                    style={{ display: 'flex'}}
+                    fill="#219ebc"
+                    paused={false}
+                    options={{
+                        height: 40,
+                        amplitude: 20,
+                        speed: 0.15,
+                        points: 5,
+                    }}
+                />
+            </div>
+            <div className='absolute left-0 bottom-0 w-full opacity-20'>
+                <Wave
+                    style={{ display: 'flex'}}
+                    fill="#8ecae6"
+                    paused={false}
+                    options={{
+                        height: 45,
+                        amplitude: 30,
+                        speed: 0.15,
+                        points: 3,
+                    }}
+                />
+            </div>
+        </div>
     )
 }
