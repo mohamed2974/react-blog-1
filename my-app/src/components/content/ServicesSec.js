095315db0a6f74d1a27cd68f3c3b769a17c11129
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-01-24 21:06:21 +0100

    Füge .vercel zur .gitignore hinzu und aktualisiere die Icons in ServicesSec.js mit Tailwind CSS-Klassen

--- my-app/src/components/content/ServicesSec.js
+++ my-app/src/components/content/ServicesSec.js
@@ -4,25 +4,22 @@ import { FaServer } from "react-icons/fa6";
 
 const Services = [
     {
-        color: 'blue',
-        icon: <AiOutlineGlobal />,
-        hoverEffekt: `hover:bg-${Services[2].color}-100`,
+        icon: <AiOutlineGlobal className="text-blue-500" />,
+        hoverEffekt: 'hover:bg-blue-100',
         title: "Webentwicklung",
         description:
             "Erstellung moderner und responsiver Websites mit React und Tailwind CSS. Egal ob Single-Page-Applications oder komplexe Weblösungen – ich sorge für intuitive Benutzeroberflächen und eine optimale Performance.",
     },
     {
-        color: 'teal',
-        icon: <FaServer />,
+        icon: <FaServer className="text-teal-500" />,
         hoverEffekt: 'hover:bg-teal-100',
         title: "Backend-Entwicklung",
         description:
             "Entwicklung skalierbarer Backends mit Node.js und Express. Ich baue robuste APIs, die eine nahtlose Verbindung zwischen Frontend und Datenbanken ermöglichen.",
     },
     {
-        color: 'purple',
-        icon: <BiBot />,
-        hoverEffekt: `hover:bg-${Services[2].color}-100`,
+        icon: <BiBot className="text-purple-500"/>,
+        hoverEffekt: 'hover:bg-purple-100',
         title: "Discord-Bots & Server-Management",
         description:
             "Programmierung benutzerdefinierter Discord-Bots mit Discord.js sowie professionelle Einrichtung und Verwaltung von Discord-Servern für Communities oder Unternehmen.",
@@ -41,7 +38,7 @@ export default function ServicesSec(){
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {Services.map((service, index) => (
                         <div key={index} className={`service-item p-6 bg-white rounded-lg shadow-md transition-transform transform hover:translate-y-[-10px] hover:shadow-lg ${service.hoverEffekt}`}>
-                            <div className={`text-4xl mb-5 text-${service.color}-500`}>{service.icon}</div>
+                            <div className="text-4xl mb-5 ">{service.icon}</div>
                             <h3 className="text-xl font-semibold mb-6">{service.title}</h3>
                             <p className="text-gray-600">{service.description}</p>
                         </div>
