e56f59c2c54c3203c6afc459e691fcce675fe7c9
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-01-26 22:23:40 +0100

    Aktualisiere Header-Komponente mit verbesserten Stilen und Layout-Anpassungen

--- my-app/src/components/app/Header.js
+++ my-app/src/components/app/Header.js
@@ -39,25 +39,25 @@ export default function Header(){
     }
     //* ######################### HEADER WEB ######################### //
     return(
-        <header className='flex flex-row justify-between font-medium items-center select-none px-4 py-4 w-full md:w-11/12 fixed md:px-11 md:mt-3 md:left-1/2 md:transform md:-translate-x-1/2 md:rounded-full' style={{
+        <header className='flex flex-row justify-between font-medium items-center select-none px-4 py-3 w-full md:w-[95%] lg:w-11/12 fixed md:px-11 md:mt-3 md:left-1/2 md:transform md:-translate-x-1/2 md:rounded-full' style={{
             backgroundColor: '#282c34',
             boxShadow: 'rgba(97, 218, 251, 0.1) 0px 0px 20px 2px',
             color: 'rgb(255, 255, 255)',
             zIndex: 100
             }}>
             {/*######################### logo #########################*/}
-            <div className='md:w-2/12'>
-                <h1 className='text-xl md:text-2xl'>
+            <div className='w-fit'>
+                <h1 className='text-xl lg:text-2xl flex flex-row items-baseline flex-nowrap'>
                     <span>Mohamed</span>
                     <div className='w-2 h-2 rounded-full bg-sky-500 inline-block'></div>
                 </h1>
             </div>
             {/*######################### navigation #########################*/}
-            <Uls style={'hidden md:justify-between md:flex md:w-fit'} listItemStyle={`${hoverEffectNavList} p-1 md:mx-2`} array={list} row={true}/>
-            <div className='w-1/5 flex justify-end md:w-fit '>
+            <Uls style={'hidden justify-between md:flex min-w-fit'} listItemStyle={`${hoverEffectNavList} py-1 mx-2 lg:mx-4 text-sm lg:text-base`} array={list} row={true}/>
+            <div className='flex justify-end w-fit '>
                 {/*######################### login #########################*/}
                 <Status className={'hidden md:flex'}>
-                    <a href='/' className='hidden w-fit md:flex md:items-center md:justify-between '>
+                    <a href='/' className='hidden w-fit md:flex items-center justify-between pointer-events-none cursor-not-allowed '>
                         <span className='mx-2'>Einloggen</span> 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
                             <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
